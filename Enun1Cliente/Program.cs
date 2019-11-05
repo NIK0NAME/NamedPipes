a65fc67b3d2f0e51be3424139099d3a1e689b10e
niko97killer <nikolayzabaleta@hotmail.com>
2019-11-05 11:29:06 +0100

    Server created and Client added

---
+++ Enun1Cliente/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using System.IO.Pipes;
+
+namespace Enun1Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int posX, posY;
+            NamedPipeClientStream npcs = new NamedPipeClientStream(".", "mi_pipe", PipeDirection.InOut);
+            npcs.Connect();
+
+            StreamReader s_read = new StreamReader(npcs);
+            StreamWriter s_write = new StreamWriter(npcs);
+        }
+    }
+}
--- Enun1Servidor/Program.cs
+++ Enun1Servidor/Program.cs
@@ -18,10 +18,34 @@ namespace Enun1Servidor
             NamedPipeServerStream npss = new NamedPipeServerStream("mi_pipe", PipeDirection.InOut);
             npss.WaitForConnection();
 
+            Console.WriteLine("Introducir coordenadas");
+            Console.Write("Posicion X: ");
+            posX = int.Parse(Console.ReadLine());
+
+            Console.Write("Posicion Y: ");
+            posY = int.Parse(Console.ReadLine());
+
             StreamReader s_read = new StreamReader(npss);
             StreamWriter s_write = new StreamWriter(npss);
 
-            
+            s_write.WriteLine(posX);
+            s_write.WriteLine(posY);
+
+            String palabra = Console.ReadLine();
+            while(palabra.CompareTo("fin") == 0)
+            {
+                s_write.WriteLine(palabra);
+                palabra = Console.ReadLine();
+            }
+
+            posX = int.Parse(s_read.ReadLine());
+            posY = int.Parse(s_read.ReadLine());
+
+            Console.WriteLine("Coordenadas -> X: " + posX + " Y: " + posY);
+
+            s_read.Close();
+            s_write.Close();
+
         }
     }
 }
